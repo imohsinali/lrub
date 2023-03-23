8a78550100f416098292a786b294034bbf3fdb6a
Mohsin Ali <59974279+imohsinali@users.noreply.github.com>
2023-03-23 21:17:15 +0500

    Create Layout.jsx

---
+++ src/components/Home/Layout.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import Navbar from './Navbar'
+import { Context } from '../context/context'
+import BannerCarousel from './Banner'
+import { Box, Flex, Text } from '@chakra-ui/react';
+import HeroSection from './HeroSection';
+import CardGrid from './Grid';
+import Join from './Join';
+import Banner1 from './Banner1';
+import Footer from './Footer';
+
+
+export default function Layout({children}) {
+  console.log(Context)
+  return (
+    <Flex direction={"column"} justifyContent="center">
+      <Navbar />
+      <Flex
+        direction={"column"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <BannerCarousel />
+        <Flex justifyContent={"center"} direction="column">
+          <Text fontSize={"4rem"} align={"center"}>
+            Key Fetures
+          </Text>
+          <CardGrid />
+        </Flex>
+        <Flex
+          justifyContent={"center"}
+          direction="column"
+          backgroundColor={"white"}
+          zIndex={2}
+        >
+          <Text fontSize={"4rem"} align={"center"}>
+            How To Join.?
+          </Text>
+          <Join />
+        </Flex>
+        <Flex
+          justifyContent={"center"}
+          direction="column"
+          // backgroundColor={"gray.100"}
+          pb={10}
+          pt={10}
+          mt={-10}
+          zIndex={1}
+          opacity={5}
+          color={{ base: "red", med: "green", sm: "yellow", xl: "blue" }}
+          width={{ base: 400, sm: 100, md: 810, xl: "95%" }}
+          overflow={"hidden"}
+        >
+          <Text fontSize={{base:"2rem", sm: "4rem"}} align={"center"} mb={5} >
+            Development Stacks
+          </Text>
+          <Flex
+            justifyContent={"center"}
+            direction="row"
+            // color={{ base: "red", med: "green", sm: "yellow", xl: "blue" }}
+            //  width={{ base: 400, sm: 100, md: 810, xl: '100%' }}
+            // overflow={"hidden"}
+          >
+            <Banner1 />
+          </Flex>
+        </Flex>
+        <Footer />
+        {/* Rest of your home page content */}
+      </Flex>
+      <main>{children}</main>
+    </Flex>
+  );
+}
